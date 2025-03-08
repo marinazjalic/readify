5d0fa30946201d85262401dec435feae0900f1de
marinazjalic <zjalicm@uwindsor.ca>
2025-03-08 13:27:24 -0500

    newsletter update

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,18 +1,39 @@
-import { Lora } from "next/font/google";
+import { Lora, Allura } from "next/font/google";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 const lora = Lora({ subsets: ["latin"] });
+const allura = Allura({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function Newsletter() {
   return (
-    <div className="w-[30%] h-120px bg-white border-r mt-4 mr-2 flex-col justify-center text-gray-600 ml-2">
+    <div className="hidden md:block w-[30%] h-120px bg-white border-r mt-4 mr-2 flex-col justify-center text-gray-600 ml-2">
       <h3 className={`text-2xl ${lora.className} italics`}>
         Looking for your next read?
       </h3>
       <h1 className="text-sm mt-2 ">Top Book This Week</h1>
-      {/* temp container for top book */}
-      <div className="bg-olive-green-100 w-[97%] h-40 mt-2"></div>
 
-      <div className="mr-2">
+      {/* temp placeholder for top book */}
+      <div className="bg-white w-[97%] h-40 mt-2 flex">
+        <div className="h-full w-28 flex-shrink-0 relative">
+          <Image
+            src="/assets/66ddd80562024b171a6fb6cd.jpg"
+            alt="Book cover"
+            fill
+            className="object-contain object-left"
+          />
+        </div>
+
+        <div className="flex-grow pl-2 flex flex-col">
+          <h4 className="text-md font-medium">Book Title</h4>
+          <p className="text-sm text-gray-600">Author Name</p>
+        </div>
+      </div>
+
+      <div className="mr-2 border-t-2 mt-2">
         <h3 className={`${lora.className} text-lg mt-2`}>
           See what your friends are reading!
         </h3>
@@ -42,6 +63,37 @@ export default function Newsletter() {
             Set your reading goals and take part in exciting new challenges!
           </p>
         </div>
+      </div>
+
+      {/* seasonal reading lists */}
+      <div className="flex items-center justify-center border-b-2">
+        <Image
+          src="/assets/spring-icon.png"
+          alt="logo"
+          width={23}
+          height={23}
+          className="mt-1 mr-2 mb-2"
+        />
+        <Button className="bg-white hover:bg-white text-gray-500 text-xs border-none shadow-none px-0 hover:underline">
+          Discover New Releases This Spring
+        </Button>
+        <Image
+          src="/assets/spring-icon.png"
+          alt="logo"
+          width={23}
+          height={23}
+          className="mt-1 ml-2 mb-2"
+        />
+      </div>
+
+      <div className="bg-white mt-2 flex border-b-2 border-olive-green-100">
+        <div className="w-[35%]">
+          <h3 className={`text-5xl ${allura.className} mb-1`}>Sign In</h3>
+        </div>
+        <p className={`${lora.className} text-xxs w-[55%] ml-4 mt-1`}>
+          Start curating your virtual library and unlock personalized
+          recommendations.
+        </p>
       </div>
     </div>
   );
