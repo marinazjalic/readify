9e8783602ed05425b3b456e787e778101afec414
marinazjalic <zjalicm@uwindsor.ca>
2025-03-27 13:55:35 -0400

    fixed loading spinner

--- src/app/pages/books/page.tsx
+++ src/app/pages/books/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import BookDisplay from "@/components/books/BookDisplay";
-import { searchBooksByQuery } from "@/actions/books/searchBooksByQuery";
 import { useState, useEffect } from "react";
-import { BookDetails } from "@/types";
+import { BookDetails, DisplayBook } from "@/types";
 import { useSearchParams } from "next/navigation";
-import LoadingSpinner from "@/components/LoadingSpinner";
-import { DisplayBook } from "@/types";
+import { Loader2 } from "lucide-react";
+import { searchBooksByQuery } from "@/actions/books/searchBooksByQuery";
+import BookDisplay from "@/components/books/BookDisplay";
 
 export default function Books() {
   const searchParams = useSearchParams();
@@ -33,30 +32,49 @@ export default function Books() {
     fetchBooks();
   }, [filter, query, currentPage]);
 
+  //disable the scrollbar when loading
+  useEffect(() => {
+    if (isLoading) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isLoading]);
+
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
   return (
-    <div>
-      <br />
-      {isLoading && <LoadingSpinner />}
-      <div className="bg-white min-h-screen">
-        <BookDisplay
-          //mapping to type DisplayBook
-          books={
-            books.map((book) => ({
-              ...book,
-              isSaved: false,
-            })) as DisplayBook[]
-          }
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-          query={query}
-          isLoading={isLoading}
-        />
-      </div>
+    <div className="bg-white min-h-screen relative">
+      {isLoading ? (
+        <div
+          className="absolute inset-0 flex justify-center items-center"
+          style={{ marginTop: "-64px" }}
+        >
+          <Loader2 className="h-8 w-8 text-olive-green-400 animate-spin" />
+        </div>
+      ) : (
+        <div>
+          <br />
+          <BookDisplay
+            books={
+              books.map((book) => ({
+                ...book,
+                isSaved: false,
+              })) as DisplayBook[]
+            }
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handlePageChange}
+            query={query}
+            isLoading={isLoading}
+          />
+        </div>
+      )}
     </div>
   );
 }
